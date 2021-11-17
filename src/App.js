4f1cb487ff05871dc26aeaf0ebf21b80e00bbd3d
AntoineGhysens <antoine.ghsens@gmail.com>
2021-11-17 15:53:32 +0100

    Added components to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './App.scss'
+import Main from './components/Main.js';
+import './style/app.scss'
 
 const App = () => {
   return (
-    <h1>Hello World</h1>
-  )
+    <div>
+      <Main title="Pomodoro"/>
+    </div>
+  );
 }
 
-
+// export default App;
 ReactDOM.render(<App/>, document.querySelector('#root'))
