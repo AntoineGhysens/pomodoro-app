2f26879f087cd3b0278db69d1968e9e0e37d2500
AntoineGhysens <antoine.ghsens@gmail.com>
2021-11-22 10:44:01 +0100

    added prop to disable buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Button = ({name, handleClick, addClass}) => {
+const Button = ({name, handleClick, addClass, disabled}) => {
   return (
-    <button onClick={handleClick} type="button" className={addClass}>{name}</button>
+    <button onClick={handleClick} type="button" disabled={disabled} className={addClass}>{name}</button>
   )
 }
 
