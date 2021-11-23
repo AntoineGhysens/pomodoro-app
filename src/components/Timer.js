aa45006624baf1c682e38472dccce48b8893cc3d
AntoineGhysens <antoine.ghsens@gmail.com>
2021-11-23 10:31:30 +0100

    added logic to display seconds as 0X when lesser than 10

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,8 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Timer = ({minutes, seconds}) => {
+  const lessThanTen = nb => {
+    if(nb < 10){
+      return `0${nb}`
+    }
+    else {
+      return nb
+    }
+  }
+
   return (
-    <p className="pomodoro__main__timer">{minutes} : {seconds}</p>
+    <p className="pomodoro__main__timer">{minutes} : {lessThanTen(seconds)}</p>
   )
 }
 
